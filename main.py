7f25c268f9cbc7186afa56e6cab52932b1dcb82a
adri <adrifdlh@gmail.com>
2022-12-02 17:59:25 +0700

    Get channels that were used only once by each user.

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ import pandas as pd
 
 # Load the csv file and convert it to be a pandas dataframe
 df = pd.read_csv("call_logging.csv")
-# Change the time format in dataframe to make the process easier
-df["time"] = pd.to_datetime(df["time"],unit="s")
 
 
 # How many channels are typically required for a single call? 
 
 # Group by user
-user_grp = df.groupby("userId")
-# Get how many channels each user uses to call
-print(user_grp["channelName"].value_counts())
+user_grp = df.groupby(["userId"])
 
-
+# Get channels that were used only once by each user 
+print(user_grp["channelName"].value_counts().loc[lambda x: x == 1])
