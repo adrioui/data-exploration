46870d08db762ceb4c1a65280c26dbefbad14dbd
adri <adrifdlh@gmail.com>
2022-12-02 10:04:31 +0700

    Make a filter that count the total of user's calls.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+# Import the required library
 import pandas as pd
 
+# Load the csv file and convert it to be a pandas dataframe
 df = pd.read_csv("call_logging.csv")
+# Change the time format in dataframe to make the process easier
+df["time"] = pd.to_datetime(df["time"],unit="s")
 
 
 # How many channels are typically required for a single call? 
-some_cols = df.groupby("channelName")
 
 filter = (df["userId"].value_counts() == 1)
-
 print(filter)
