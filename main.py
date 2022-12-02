cadb56c2086da3c4231de04db4d3d382434cded0
adri <adrifdlh@gmail.com>
2022-12-02 08:56:56 +0700

    Pip install.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello")
+import pandas as pd
+
+df = pd.read_csv("call_logging.csv")
+
+
+# How many channels are typically required for a single call? 
+some_cols = df.groupby("channelName")
+
+filter = (df["userId"].value_counts() == 1)
+
+print(filter)
